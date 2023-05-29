006a02eb69d77bb780c28593d0ff9da2911d2bd8
WellingtonSI <12ribe@gmail.com>
2023-05-29 20:37:34 -0300

    auth signin

--- api/auth.js
+++ api/auth.js
@@ -12,5 +12,25 @@ module.exports = app => {
         const user = await app.db('users')
                 .where({email: req.body.email})
                 .first()
+
+        if(user){
+            bcrypt.compare(req.body.password,user.password, (err, isMatch) => {
+                if(err || !isMatch){
+                    return res.status(401).send()
+                }
+
+                const payload = {id : user.id}
+                req.json({
+                    name:user.name,
+                    email: user.email,
+                    token: jwt.encode(payload, authSecret),
+                })
+            })
+        }else{
+            res.status(400).send('Usuário não cadastrado!')
+        }
     }
+
+    return { signin }
+
 }
